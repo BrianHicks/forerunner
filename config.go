bcb91db90f12882913a48bc6e53592694abc6966
Brian Hicks <brian@brianthicks.com>
2014-06-12 13:49:37 -0500

    config: update flags

--- config.go
+++ config.go
@@ -9,13 +9,19 @@ import (
 )
 
 type Config struct {
-	Image           string
-	Registry        string
-	Tag             string
-	DockerEndpoint  string
-	Name            string
+	// Forerunner
 	ShutdownTimeout time.Duration
 	LogLevel        int
+
+	// Docker
+	Image          string
+	Registry       string
+	DockerEndpoint string
+	Name           string
+
+	// Etcd
+	EtcdHosts    string
+	ConfigPrefix string
 }
 
 func NewConfig() *Config {
@@ -23,10 +29,11 @@ func NewConfig() *Config {
 
 	pflag.StringVar(&c.Image, "image", "", "docker image to run")
 	pflag.StringVar(&c.Registry, "registry", "", "docker registry to contact")
-	pflag.StringVar(&c.Tag, "tag", "latest", "tag to pull for image")
 	pflag.StringVar(&c.Name, "name", "", "name of docker container")
 	pflag.StringVar(&c.DockerEndpoint, "docker-endpoint", "unix:///var/run/docker.sock", "docker socket to use")
 	pflag.DurationVar(&c.ShutdownTimeout, "shutdown-timeout", 5*time.Second, "how long to wait after intterupt before forcibly stopping")
+	pflag.StringVar(&c.EtcdHosts, "etcd-hosts", "http://127.0.0.1:4001", "comma-separated list of etcd hosts to connect to")
+	pflag.StringVar(&c.ConfigPrefix, "config-prefix", "/forerunner/", "etcd prefix to pull configuration from")
 
 	logLevel := pflag.String("log-level", "info", "level to log at (debug, info, warning, error, fatal)")
 
